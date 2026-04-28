1327e6225948b6c1583a203843e1e512263ef792
Sofi <sofiullah.work@gmail.com>
2026-04-28 22:12:48 +0700

    models & migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,10 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role', 'active'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -27,6 +29,42 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => 'string',
+            'order_count' => 'integer',
+            'can_edit' => 'boolean',
         ];
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    protected function ordersCount(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->orders()->count(),
+        );
+    }
+
+    protected function canEdit(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                $user = Auth::user();
+
+                if ($user) {
+                    if ($user->role == 'administrator') {
+                        return true;
+                    } elseif ($user->role == 'manager') {
+                        return $value->role == 'user' || $value->id == $user->id;
+                    } else {
+                        return $value->id == $user->id;
+                    }
+                }
+
+                return false;
+            },
+        );
     }
 }
---
+++ database/migrations/2026_04_28_144807_add_role_ative_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('active')->after('password')->default(true);
+            $table->enum('role', ['user', 'manager', 'administrator'])->after('password')->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('active');
+        });
+    }
+};
